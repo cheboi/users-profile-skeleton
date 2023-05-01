ecaf3ed425e4219894c2e6cd3116fccafdb5a909
cheboi <moscheb96gmail.com>
2023-05-01 09:29:35 +0300

    intergrated use api

--- server/express.js
+++ server/express.js
@@ -9,11 +9,6 @@ import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
-// modules for server side rendering
-// import React from 'react'
-// import ReactDOMServer from 'react-dom/server'
-// import MainRouter from './../client/MainRouter'
-// import { StaticRouter } from 'react-router-dom'
 
 import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
 import theme from './../client/theme'
